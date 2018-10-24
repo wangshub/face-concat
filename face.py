6870abb21f8e720fcd49fe27f4fac97f33a7d0f6
wangshub <easternslope@yeah.net>
2018-10-24 20:39:01 +0800

    feat: mid point of coors

--- face.py
+++ face.py
@@ -1,27 +1,49 @@
 from PIL import Image, ImageDraw
 import face_recognition
 
-# Load the jpg file into a numpy array
-image = face_recognition.load_image_file("face/trump-1.jpg")
 
-# Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
+def midpoint(coors):
+    """
+    get midpoint of coors
+    :param coors:[(x, y), ...]
+    :return:
+    """
+    x, y = 0, 0
+    for coor in coors:
+        x += coor[0]
+        y += coor[1]
+    x, y = int(x / len(coors)), int(y / len(coors))
+    return x, y
 
-print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
 
-# Create a PIL imagedraw object so we can draw on the picture
-pil_image = Image.fromarray(image)
-d = ImageDraw.Draw(pil_image)
+if __name__ == '__main__':
+    # Load the jpg file into a numpy array
+    image = face_recognition.load_image_file("face/trump-1.jpg")
 
-for face_landmarks in face_landmarks_list:
+    # Find all facial features in all the faces in the image
+    face_landmarks_list = face_recognition.face_landmarks(image)
 
-    # Print the location of each facial feature in this image
-    for facial_feature in face_landmarks.keys():
-        print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+    print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
 
-    # Let's trace out each facial feature in the image with a line!
-    for facial_feature in face_landmarks.keys():
-        d.line(face_landmarks[facial_feature], width=5)
+    # Create a PIL imagedraw object so we can draw on the picture
+    pil_image = Image.fromarray(image)
+    d = ImageDraw.Draw(pil_image)
 
-# Show the picture
-pil_image.show()
+    for face_landmarks in face_landmarks_list:
+
+        # Print the location of each facial feature in this image
+        for facial_feature in face_landmarks.keys():
+            print("{} : {}".format(facial_feature, face_landmarks[facial_feature]))
+
+        # Let's trace out each facial feature in the image with a line!
+        for facial_feature in face_landmarks.keys():
+            d.line(face_landmarks[facial_feature], width=1)
+
+
+
+    # Show the picture
+    pil_image.show()
+
+    # area = (400, 400, 800, 800)
+    # cropped_img = pil_image.crop(area)
+    # cropped_img.show()
