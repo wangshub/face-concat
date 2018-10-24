bc283be49394f6231e8f5924f58d895e7f340422
wangshub <easternslope@yeah.net>
2018-10-24 20:59:48 +0800

    feat: image size

--- face.py
+++ face.py
@@ -22,27 +22,30 @@ if __name__ == '__main__':
 
     # Find all facial features in all the faces in the image
     face_landmarks_list = face_recognition.face_landmarks(image)
-
     print("I found {} face(s) in this photograph.".format(len(face_landmarks_list)))
+    num_faces = len(face_landmarks_list)
 
-    # Create a PIL imagedraw object so we can draw on the picture
-    pil_image = Image.fromarray(image)
-    d = ImageDraw.Draw(pil_image)
+    if num_faces == 1:
+        # Create a PIL imagedraw object so we can draw on the picture
+        pil_image = Image.fromarray(image)
+        img_size = pil_image.size
+        d = ImageDraw.Draw(pil_image)
 
-    for face_landmarks in face_landmarks_list:
+        for face_landmarks in face_landmarks_list:
+            for facial_feature in face_landmarks.keys():
+                print("{} : {}".format(facial_feature, face_landmarks[facial_feature]))
 
-        # Print the location of each facial feature in this image
-        for facial_feature in face_landmarks.keys():
-            print("{} : {}".format(facial_feature, face_landmarks[facial_feature]))
+        face_landmarks = face_landmarks_list[0]
+        centre_nose_bridge = midpoint(face_landmarks['nose_bridge'])
 
-        # Let's trace out each facial feature in the image with a line!
-        for facial_feature in face_landmarks.keys():
-            d.line(face_landmarks[facial_feature], width=1)
+        d.point(centre_nose_bridge, fill='blue')
+        pil_image.show()
 
-
+    else:
+        print('multi face not support or empty face')
 
     # Show the picture
-    pil_image.show()
+    # pil_image.show()
 
     # area = (400, 400, 800, 800)
     # cropped_img = pil_image.crop(area)
